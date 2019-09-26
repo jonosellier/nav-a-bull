60342d3a15b2680f291190660d764448659a0f76
Jono Sellier <jonosellier@gmail.com>
2019-09-26 17:24:09 -0400

    fixed a small security issue

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
+//Appdata object to hold api keys and other as-needed information
+const appData = {
+    apiKey: process.env.MAPS_API_KEY
+};
+
+//EJS view engine (think of it sort of like PHP but it doesnt make you want the sweet release of death)
 app.set('view engine', 'ejs');
-app.use(express.static('staticContent'));
-app.get('/', (req, res) => res.render('index', { foo: "foo" }));
 
+//sets staticContent folder as the root for all static content (ie .../staticContent/app.js is accessed as ./app.js)
+app.use(express.static('staticContent'));
+
+//when a GET request is made for the root we render the index.ejs page passing the API key in to be rendered as an HTML string response
+app.get('/', (req, res) => res.render('index', { apikey: appData.apiKey }, function(err, html) {
+    console.log(`Key = ${process.env.MAPS_API_KEY}`);
+    res.send(html);
+}));
+
+
+//start the server
 app.listen(port, () => console.log(`Listening on port ${port}!`));
