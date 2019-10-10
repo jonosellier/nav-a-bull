b166b8a7924483b87ffee213d3bf7b08f19b46d2
Jono Sellier <jonosellier@gmail.com>
2019-10-10 12:00:08 -0400

    added login page at /login (no link yet)

--- staticContent/app.js
+++ staticContent/app.js
@@ -139,3 +139,12 @@ function showHideNav() {
 function pageLoaded() {
     showHideNav();
 }
+
+function moveTo(pos) {
+    if (pos == 0) document.getElementsByClassName("doublewide")[0].style.marginLeft = "0";
+    if (pos == 1) document.getElementsByClassName("doublewide")[0].style.marginLeft = "-100%";
+    for (let e of document.getElementsByClassName('text-btn')) {
+        e.classList.add('inactive');
+    }
+    document.getElementsByClassName('text-btn')[pos].classList.remove('inactive');
+}
